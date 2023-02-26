f1b3b266044c60dd28f9df3cc775e090bfbccac1
Baris34 <46277882+Baris34@users.noreply.github.com>
2023-02-27 00:22:14 +0300

    fibonacci

---
+++ src/Examples/Fibonacci.java
@@ -0,0 +1,15 @@
+package Examples;
+
+public class Fibonacci {
+
+	public static void main(String[] args) {
+		int fib=0,fib1=1,temp;
+		for (int i = 0; i < 9; i++) {
+			temp=fib+fib1;
+			fib=fib1;
+			fib1=temp;
+			System.out.println(fib1);
+		}
+	}
+
+}
